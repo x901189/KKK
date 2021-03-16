3abd19a11f63b0033cdc4bf68f9610b8a5db8af9
kelvin <kelvin.brown@capita.com>
2021-03-16 10:27:24 +0000

    Stuff

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//
+// ID :  $Id$
+//
 namespace KKK
 {
     public class Program
